7a0cc3cd410fcda7e529681bd7a44a362812843e
Miloš Procházka <milos.proch@gmail.com>
2021-10-23 07:17:56 +0200

    test

--- ..win-bat/git-push.sh
+++ ..win-bat/git-push.sh
@@ -1,16 +1,23 @@
 #!/bin/sh
+read -p "Komentar ke commitu:" comment
+
 cd ..
-if [[ $# -eq 0 ]] ; then
-    echo Syntax: git-push ""message""
+
+if [[ "$comment" == "" ]] ; then
+    read -t 10 -p "Komentar musi byt zadan" none
     exit 0
 fi
+
+
 
 dart-format ./ 2
 dart-prep --enable-all ./
 git add --all
-git commit --all -m $1
+git commit --all -m "$comment"
 git push origin master --force
-pause
+
+read -p "Kontrola ulozeni gitu"
+
 git gc
 git gc --aggressive
 git prune
